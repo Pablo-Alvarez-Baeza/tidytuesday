7f868ddc8ff924a0068b7e683d7c121dca706aa2
Pablo Alvarez Baeza <pablo.alvarez.baeza@gmail.com>
2022-01-15 14:00:56 +0100

    Final updates

--- 2022/week_2/2022_2_bees.R
+++ 2022/week_2/2022_2_bees.R
@@ -1,14 +1,11 @@
 library(pacman)
-p_load(tidyverse, tidytuesdayR, geofacet, ggtext, showtext)
+p_load(tidyverse, tidytuesdayR, geofacet, ggtext, showtext, cowplot)
 
 font_add_google("Lato")
 showtext_auto()
 
 tt <- tidytuesdayR::tt_load('2022-01-11')
-
 colony <- tt$colony
-
-colony |> glimpse()
 
 df <- colony |> 
   select(year, months, state, colony_lost_pct, colony_reno_pct) |> 
@@ -27,53 +24,61 @@ df <- colony |>
   add_row(state = c("Nevada", "Alaska", "New Hampshire", "Rhode Island", "Delaware"),
           consecutive_year = rep(5), group = rep("D", 5))
 
- p <-  df |> 
-    ggplot(aes(x = 1, y = 1, fill = group)) +
-    geom_col(show.legend = FALSE) +
-    facet_geo(~ state) +
-    geom_text(aes(label = state, x = 1, y = .8, ),
-              size = 1.5, fontface = "bold") +
-    coord_cartesian(expand = c(0, 0), clip = "off") +
-    scale_fill_manual(values = c("#face48","#f0f0f0","#ed4e83", "grey95")) +
-    labs(title = "Hawaiian bees thrive",
+
+plot <- df |> 
+  ggplot(aes(x = 1, y = 1, fill = group)) +
+  geom_col(show.legend = FALSE) +
+  facet_geo(~ state) +
+  geom_text(aes(label = state, x = 1, y = 5),
+              size = 2.3, fontface = "bold") +
+  coord_cartesian(expand = c(0, 0), clip = "off") +
+  scale_fill_manual(values = c("#2596be","#f2f2f2","#ed4e83", "grey50")) +
+  labs(title = "Hawaiian bees thrive",
          subtitle = "States' consecutive percentage of bee colonies lost vs. renovated, 2015 - 2021",
-         caption = "Visualization by Pablo Alvarez | Data from United States Department of Agriculture (USDA)") +
-    theme_void(base_family = "Lato") +
-    theme(plot.margin = margin(20, 40, 20, 40),
-          panel.spacing = unit(0, "lines"),
-          panel.border = element_rect(color = "white", fill = NA, size = .3),
-          strip.text = element_blank(),
-          strip.background = element_blank(),
-          plot.title = element_text(
-            size = 12,
-            margin = margin(t = 15),
-            face = "bold",
-            family = "Lato"
+         caption = "Visualization by Pablo Alvarez | Data from United States Department of Agriculture (USDA). Data is not available for the states colored in dark grey") +
+  theme_void(base_family = "Lato") +
+  theme(panel.background = element_rect(fill = "white", color = "white"),
+        plot.background = element_rect(fill = "white", color = "white"),
+        plot.margin = margin(20, 40, 20, 80),
+        panel.spacing = unit(0, "lines"),
+        panel.border = element_rect(color = "white", fill = NA, size = .3),
+        strip.text = element_blank(),
+        strip.background = element_blank(),
+        legend.position = "top",
+        plot.title = element_text(
+          size = 18,
+          margin = margin(t = 15),
+          face = "bold",
+          family = "Lato Black (900)"
           ),
           plot.subtitle = element_text(
-            size = 10,
-            margin = margin(t = 20, b = 20)
+            size = 14,
+            margin = margin(t = 10, b = 40),
+            family = "Lato"
           ),
           plot.caption = element_text(
             color = "grey50",
+            family = "Lato",
             hjust = .5,
             size = 10,
-            margin = margin(t = 15, b = 5)
+            margin = margin(t = 40, b = 5)
           )
     )
  
- 
-#A data frame with labels for each facet
-f_labels <- data.frame(state = c("Hawaii"), label = "<span style='color:green'>Hawaii</span>")
- 
-p +
-   annotate(x = 1, y = 1, geom = "text", data = f_labels)
- 
- 
-p +
-  annotate(data = df |> filter(state == "Hawaii"), geom = "text", label = "a", x = 1, y = 1))
-  
-  ggdraw(p) + draw_label("<span style='color:green'>Hawaii</span>" , x = 0.1, y = 0.35)
-  subtitle = "<span style='color:green'>Hawaii</span> is the only state where the percentage of colonies renovated is greater than the percentage of colonies lost each year.<br>In contrast, <span style='color:red'>Minnesota, Illinois, and Virginia</span> have a higher percentage of colonies lost than renovated every year.",
-  
-  
+ggdraw(plot) + 
+  draw_label(label = "Hawaii is the only state\nwhere the percentage of colonies\nrenovated is greater than\nthe percentage of colonies lost each year",
+             x = .035, y = 0.275,
+             size = 10, hjust = 0,
+             lineheight = 1,
+             fontfamily = "Lato",
+             fontface = "bold",
+             color = "#2596be") +
+  draw_label(label = "Minnesota, Illinois, and Virginia\nhave a higher percentage\nof colonies lost than\nrenovated every year",
+             x = .75, y = 0.32,
+             size = 10, hjust = 0,
+             lineheight = 1,
+             fontfamily = "Lato",
+             fontface = "bold",
+             color = "#ed4e83") 
+
+ggsave("tidytuesday_2022_w2.png", width = 10, height = 8.5, units = "in", dpi = 320)
