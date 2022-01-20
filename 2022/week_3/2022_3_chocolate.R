53e892ce5819e27e99a0032122dec674231325d2
Pablo Alvarez Baeza <pablo.alvarez.baeza@gmail.com>
2022-01-21 00:22:08 +0100

    Update 2022_3_chocolate.R

--- 2022/week_3/2022_3_chocolate.R
+++ 2022/week_3/2022_3_chocolate.R
@@ -6,64 +6,52 @@ p_load(tidyverse, tidytuesdayR, ggalluvial, gapminder, ggtext, showtext)
 tt <- tidytuesdayR::tt_load('2022-01-18')
 chocolate <- tt$chocolate
 
-chocolate |> glimpse()
-gapminder <- gapminder |> 
-  select(country, continent)
 
-continents_summary <- chocolate |> 
-  left_join(gapminder, by = c("country_of_bean_origin" = "country")) |>
-  relocate(continent_of_bean_origin = continent, .after = country_of_bean_origin) |> 
-  group_by(continent_of_bean_origin) |> 
+countries_summary <- chocolate |> 
+  group_by(country_of_bean_origin) |> 
   count() |> 
   ungroup() |> 
   mutate(total = sum(n),
-         perc_continent = round(n / total, 2)) |> 
-  select(continent_of_bean_origin, perc_continent)
+         perc_country = round(n / total, 2)) |>
+  arrange(desc(perc_country)) |> 
+  mutate(cumsum_perc = cumsum(perc_country),
+         group = factor(if_else(cumsum_perc <= .45, 1, 2))) |> 
+  select(country_of_bean_origin, perc_country, group)
+
 
 df <- chocolate |> 
-  left_join(gapminder, by = c("country_of_bean_origin" = "country")) |>
-  relocate(continent_of_bean_origin = continent, .after = country_of_bean_origin) |> 
-  group_by(continent_of_bean_origin) |> 
+  group_by(country_of_bean_origin) |> 
   count(company_location, sort = TRUE) |> 
-  mutate(sum = sum(n),
-         perc = round(n / sum, 2),
-         continent_of_bean_origin = factor(continent_of_bean_origin),
-         company_location = factor(company_location),
-         group = factor(if_else(continent_of_bean_origin == "Americas", 1, 2))) |>   ungroup() |> 
-  left_join(continents_summary, by = "continent_of_bean_origin") |> 
-  mutate(continent_of_bean_origin = fct_reorder(continent_of_bean_origin, desc(perc_continent))) 
+  ungroup() |> 
+  left_join(countries_summary, by = "country_of_bean_origin") |> 
+  mutate(country_of_bean_origin = fct_reorder(country_of_bean_origin, desc(perc_country)))
 
-countries_text <- chocolate |> 
-  left_join(gapminder, by = c("country_of_bean_origin" = "country")) |> 
-  filter(continent == "Americas") |> 
-  count(country_of_bean_origin, sort = TRUE) |> 
-  mutate(text_size = rank(n))
-
-df |> 
-  ggplot(aes(axis1 = continent_of_bean_origin, axis2 = company_location, y = n)) +
+ 
+ggplot(df,
+       aes(axis1 = country_of_bean_origin, axis2 = company_location, y = n, label = country_of_bean_origin)) +
   geom_alluvium(aes(fill = group), show.legend = FALSE, na.rm = TRUE, alpha = .65) +
-  scale_y_continuous(expand = c(0, 0)) +
+  #geom_stratum(alpha = .5)  +
+  #geom_text(stat = "stratum", size = 2, min.y = 73, aes(label = after_stat(stratum))) +
+  scale_y_continuous(breaks = seq(0, 2500, by = 100),
+                     expand = c(0, 0)) +
   scale_x_continuous(expand = c(0, 0)) +
-  scale_fill_manual(values = c("grey25", "white")) +
-  labs(title = "<span style='font-size:36pt'>**74%**</span> of the cacao beans in the world are distributed by the Americas",
-       caption = "Visualization by Pablo Alvarez | Data from Flavors of Cacao") +
-  annotate(geom = "text", x = .8, y = 24000, label = "Venezuela", hjust = 1, size = 3, color = "white") +
-  annotate(geom = "text", x = .8, y = 23000, label = "Peru", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 22000, label = "Dominican Republic", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 21000, label = "Ecuador", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 20000, label = "Nicaragua", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 19000, label = "Bolivia", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 18000, label = "Colombia", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 17000, label = "Brazil", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 16000, label = "Guatemala", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 15000, label = "Mexico", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 14000, label = "Costa Rica", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 13000, label = "Honduras", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 12000, label = "Jamaica", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 11000, label = "Cuba", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 10000, label = "Panama", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 9000, label = "Puerto Rico", hjust = 1, size = 3) +
-  annotate(geom = "text", x = .8, y = 8000, label = "El Salvador", hjust = 1, size = 3) +
+  coord_cartesian(clip = "off") +
+  annotate(geom = "text", x = 1, y = 2410, label = "Peru", hjust = .5, size = 4, color = "white", fontface = "bold") +
+  annotate(geom = "text", x = 1, y = 2160, label = "Venezuela", hjust = .5, size = 4, color = "white", fontface = "bold") +
+  annotate(geom = "text", x = 1, y = 1930, label = "Dominican Republic", hjust = .5, size = 4, color = "white", fontface = "bold") +
+  annotate(geom = "text", x = 1, y = 1700, label = "Ecuador", hjust = .5, size = 4, color = "white", fontface = "bold") +
+  annotate(geom = "text", x = 1, y = 1500, label = "Madagascar", hjust = .5, size = 4, color = "white", fontface = "bold") +
+  annotate(geom = "text", x = 2, y = 1700, label = "United States", hjust = .5, size = 4, color = "black", fontface = "bold") +
+  annotate("segment", x = .9, xend = 1.1, y = 2525, yend = 2525, colour = "white") +
+  annotate("segment", x = .9, xend = 1.1, y = 2290, yend = 2290, colour = "white") +
+  annotate("segment", x = .9, xend = 1.1, y = 2025, yend = 2025, colour = "white") +
+  annotate("segment", x = .9, xend = 1.1, y = 1805, yend = 1805, colour = "white") +
+  annotate("segment", x = .9, xend = 1.1, y = 1590, yend = 1590, colour = "white") +
+  annotate("segment", x = .9, xend = 1.1, y = 1405, yend = 1405, colour = "white") +
+  annotate("segment", x = 1.9, xend = 2.1, y = 1110, yend = 1110, colour = "white") +
+  annotate("segment", x = 1.9, xend = 2.1, y = 2250, yend = 2250, colour = "white") +
+  scale_fill_manual(values = c("#654321", "grey75")) +
+  labs(title = "<span style='font-size:36pt'>**45%**</span> of the cacao beans in the world are distributed by these countries") +
   theme_void() +
   theme(plot.margin = margin(10, 40, 10, 40),
         plot.background = element_rect(fill = "black", color = "black"),
@@ -73,7 +61,7 @@ df |>
           size = 16, 
           hjust = .5,
           face = "bold",
-          margin = margin(t = 15)
+          margin = margin(b = 40)
         )
   )
 
