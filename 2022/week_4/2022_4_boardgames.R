40c0ec869b50304ba8626c34b839017826484488
Pablo Alvarez Baeza <pablo.alvarez.baeza@gmail.com>
2022-01-25 23:07:59 +0100

    Week 4

---
+++ 2022/week_4/2022_4_boardgames.R
@@ -0,0 +1,61 @@
+library(pacman)
+p_load(tidyverse, tidytuesdayR)
+
+tt <- tidytuesdayR::tt_load('2022-01-25')
+
+ratings <- tt$ratings
+details <- tt$details
+
+ratings |> glimpse()
+details |> glimpse()
+
+ratings |> 
+  slice_min(rank, n = 10) |> View()
+
+df <- inner_join(ratings, details, by = "id")
+
+df |> 
+  group_by(playingtime, r) |> 
+  arrange(desc(average)) |> View()
+
+
+df |> 
+  ggplot(aes(year, average)) +
+  geom_line()
+
+details |> 
+  mutate(minplayers = factor(minplayers)) |> 
+  group_by(minplayers) |> 
+  count(yearpublished, sort = TRUE) |> 
+  filter(yearpublished >= 1900,
+         yearpublished <= 2021) |>
+  ungroup() |> 
+  ggplot(aes(yearpublished, n, color = minplayers)) +
+  geom_line() +
+  scale_x_continuous(breaks = seq(1900, 2025, 1))
+
+ratings |> 
+  filter(year >= 1900,
+         year <= 2021,
+         str_detect(name, "Monopoly")) |> 
+  group_by(year) |> 
+  #count(year, sort = TRUE) |> 
+  ungroup() |> 
+  ggplot(aes(year, average)) +
+  geom_line() +
+  scale_x_continuous(breaks = seq(1900, 2025, 1))
+
+
+ratings |> 
+  filter(year >= 1900,
+         year <= 2021) |> 
+  group_by(year) |> 
+  #count(year, sort = TRUE) |> 
+  summarize(mean = mean(average)) |> 
+  ungroup() |> 
+  ggplot(aes(year, mean)) +
+  geom_line() 
+  
+
+details |> 
+  group_by()
